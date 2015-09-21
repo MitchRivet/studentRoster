72b0cf133d8719334f8b3bb1d6b2c71ba03871a8
Mitch Rivet <student@epicodus.com>
2015-09-21 09:47:22 -0700

    initial commit

---
+++ js/studentController.js
@@ -0,0 +1,11 @@
+
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
+  $scope.students = [
+    {name: "Stacy Stiles"},
+    {name: "Sam Thesham"},
+    {name: "BigBoy Bobby"},
+    {name: "Steve Bruel"},
+    {name: "Barack Obama"},
+    {name: "Omar Butterfield"}
+  ]
+});
