1ba29bba21518bf334faa5c0e1d26291da9354f8
Mitch Rivet <student@epicodus.com>
2015-09-21 10:27:24 -0700

    add edit student name

--- js/studentController.js
+++ js/studentController.js
@@ -6,6 +6,16 @@ studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
     {name: "BigBoy Bobby"},
     {name: "Steve Bruel"},
     {name: "Barack Obama"},
-    {name: "Omar Butterfield"}
-  ]
+    {name: "Omar Butterfield"}];
+
+  $scope.addStudent = function() {
+    $scope.students.push({ name: $scope.studentName });
+    $scope.studentName = null;
+  };
+
+  $scope.deleteStudent = function(student) {
+    var index = $scope.students.indexOf(student);
+    $scope.students.splice(index, 1); 
+  }
+
 });
